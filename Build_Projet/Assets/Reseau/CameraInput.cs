af7f0d99dea8e593a9fd876915612d66064e0696
Nathan Urbain <nathan.urbain@etu.unistra.fr>
2017-04-20 12:28:54 +0200

    Merge Reseau

---
+++ Build_Projet/Assets/Reseau/CameraInput.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class CameraInput : NetworkBehaviour {
+
+	//Objet camera
+	public Camera camera;
+	public GameObject vaisseau;
+	private CameraReseau changement;
+
+
+	//variable pour camera 
+	public Ray viewligne;
+	public Vector2 SourisPos;
+	protected bool suivreJoueur;
+	protected bool straightFollow;
+
+	//Variable de player 
+	private Transform _transformplayer;
+	private Vector3 _vaisseaupos;
+
+	//Variable de marge de deplacement
+	public float hauteur = 3f;
+	public float distance = 50f;
+	public float marge = 0.00010f;
+
+	// Use this for initialization
+	void Start () {
+		suivreJoueur = true;
+		straightFollow = false;
+		_transformplayer = vaisseau.transform;
+		changement = transform.GetComponent<CameraReseau> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (!isLocalPlayer) {
+			return;
+		}
+			
+		viewligne = camera.ScreenPointToRay (SourisPos);
+
+		if(!suivreJoueur && !straightFollow)
+			lookVaisseau ();
+		if (straightFollow && suivreJoueur)
+			FollowStraight();
+		if(suivreJoueur && !straightFollow)
+			SmoothFollow();
+	}
+
+	/*void LateUpdate(){
+		if(!suivreJoueur && !straightFollow)
+			lookVaisseau ();
+		if (straightFollow && suivreJoueur)
+			FollowStraight();
+		if(suivreJoueur && !straightFollow)
+			SmoothFollow();
+	}*/
+
+
+	void SmoothFollow(){
+		changement.CmdSmoothFollow (hauteur, distance);
+	}
+
+	void FollowStraight(){
+		//Le joueur suit le curseur avec delai
+		changement.CmdFollowStraight(hauteur, distance);
+	}
+
+	void lookVaisseau()
+	{
+		changement.CmdlookVaisseau ();
+	}
+
+	void setSuivre(bool b)
+	{
+		suivreJoueur = b;
+	}
+
+	void setStraightFollow(bool b)
+	{
+		straightFollow = b;
+	}
+}
---
+++ Build_Projet/Assets/Reseau/Spawn/placement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class placement : NetworkBehaviour {
+
+	public GameObject[] spawnPointsRed;
+	public GameObject[] spawnPointsBlue;
+
+	void Start() {
+
+		spawnPointsBlue = GameObject.FindGameObjectsWithTag("SpawnBlue");
+		spawnPointsRed = GameObject.FindGameObjectsWithTag("SpawnRed");
+
+		if (!isServer)
+			Cmdreplacement (GameObject.FindGameObjectWithTag ("equipe").GetComponent<team_choice> ().team_v);
+	} 
+
+	void Update()
+	{ 
+		
+	}
+
+	[Command]
+	public void Cmdreplacement (int team) {
+
+
+
+		if (team == 1) {
+			GetComponent<Transform> ().position = spawnPointsBlue[Random.Range (0, spawnPointsBlue.Length)].transform.position;
+		}
+
+		if (team == 2) {
+			GetComponent<Transform> ().position = spawnPointsRed[Random.Range (0, spawnPointsRed.Length)].transform.position;
+
+		}
+
+		Rpcclientremplacement (team);
+	}
+
+	[ClientRpc]
+	public void Rpcclientremplacement (int team)
+	{
+
+		if (team == 1) {
+			GetComponent<Transform> ().position = spawnPointsBlue[Random.Range (0, spawnPointsBlue.Length)].transform.position;
+		}
+
+		if (team == 2) {
+			GetComponent<Transform> ().position = spawnPointsRed[Random.Range (0, spawnPointsRed.Length)].transform.position;
+		}
+	}
+}
