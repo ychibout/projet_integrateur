a865aba78a85c162b76bc5853e98aff9252381f8
Finnell-PC\Finnell <lambrecht.louis@yahoo.fr>
2017-05-09 14:35:51 +0200

    add menu pause script

---
+++ Scripts_a_Utiliser/MenuPause.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuPause : MonoBehaviour {
+
+	private bool isPaused = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Escape))
+			isPaused = !isPaused;
+		}
+
+	void OnGUI(){
+		if (isPaused) {
+			if (GUI.Button (new Rect (Screen.width / 2 - 30, Screen.height / 2 - 20, 80, 40), "Continuer")) {
+				isPaused = false;
+			}
+			if (GUI.Button (new Rect (Screen.width / 2 - 30, Screen.height / 2 + 40, 80, 40), "Quitter")) {
+				Application.Quit ();
+			}
+		}
+	}
+}
