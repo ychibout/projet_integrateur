64b7fea2d037cf4178b6aa60018ec10c2a418c91
cendenmann <c.endenmann@gmail.com>
2017-05-10 14:36:10 +0200

    Ajout script fin jeu + modification limitsMap

---
+++ Scripts_a_Utiliser/Fin_jeu_Timer/Croiseur.cs
@@ -0,0 +1,74 @@
+﻿/*
+ * Script gérant la fin d'une partie
+ * Pour l'utiliser, rajouter un canvas end_game contenant un panel qui contient un texte (voir code)
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class Croiseur : NetworkBehaviour {
+	// Global variables
+	public float LIFE = 100.0f;
+
+	// EndGame Panel
+	public GameObject end_panel;
+
+	// Player
+	public GameObject player;
+
+	// Life variable
+	private float life;
+
+	private float CountDownTest = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+		life = LIFE;
+		end_panel.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Fonction de test *à enlever par la suite*
+		// Destruction du croiseur utilisant ce script au bout d'un certain temps
+		if (CountDownTest <= 0.0f)
+			gameObject.GetComponent<Croiseur> ().TakeDamage (20.0f);
+		else
+			CountDownTest -= 0.1f * Time.deltaTime;
+	}
+
+	// Fonction TakeDamage
+	public void TakeDamage(float damage) {
+		if (life <= 0.0f)
+			return;
+		life -= damage;
+		if (life <= 0.0f) {
+			RpcOnDestroyed();
+		} else {
+
+		}
+	}
+
+
+	// Fonction affichant le message de victoire ou de défaite en fonction du joueur
+	// Pour modifier le texte du canvas, on prend son premier enfant (panel) puis l'enfant de celui-ci (text)
+	[ClientRpc]
+	public void RpcOnDestroyed() {
+		// Recherche du vaisseau du joueur
+		player = GameObject.Find ("Player");
+		// Arret jeu
+		Time.timeScale = 0.0f;
+		// Affichage
+		end_panel.SetActive(true);
+		if(player.tag != gameObject.tag) {
+			end_panel.SetActive (true);
+			end_panel.transform.GetChild(0).transform.GetChild(0).GetComponent<Text>().text = "YOU WIN!";
+		} else {
+			end_panel.SetActive (true);
+			end_panel.transform.GetChild(0).transform.GetChild(0).GetComponent<Text>().text = "YOU LOSE!";
+		}
+	}
+}
---
+++ Scripts_a_Utiliser/Fin_jeu_Timer/limitsMap.cs
@@ -0,0 +1,87 @@
+﻿/* 
+ * A changer:
+ *  - Fonction outOfMap()
+ * 
+ * A utiliser:
+ *  - Ajouter en tant que deuxième enfant de messOut un texte (qui sera modifié)
+ */
+
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class limitsMap : MonoBehaviour {
+
+	public GameObject messOut; // message pour dire au joueur de faire demi tour
+	public int playerOut = 0; // verifier si le joueur est dehors
+	public GameObject arrow; // fleche indique direction pour retourner dans la map
+	public Transform target; // cible vers laquelle la fleche regarde
+
+	public IEnumerator co;
+
+	/*********************************************************************
+	************************* START & UPDATE *****************************
+	*********************************************************************/
+
+	void Start () {
+		messOut.SetActive (false);
+		arrow.SetActive (false);
+
+	}
+
+	void Update(){
+		arrow.transform.LookAt (target); // on dirige fleche vers centre de la map
+	}
+
+
+	/*********************************************************************
+	************************** FONCTIONS *********************************
+	*********************************************************************/
+
+	// Lorsque le joueur sort de la map
+	private void OnTriggerExit(Collider collision){
+
+		// On verifie qu'il s'agit du joueur
+		if (collision.tag == "Equipe1" || collision.tag == "Equipe2") {
+			
+			messOut.SetActive (true); // affiche message
+			arrow.SetActive(true); // affiche flèche
+			// routine pour afficher message quitte partie au bout de 5 sec
+			co = outOfMap();
+			StartCoroutine(co); 
+			playerOut = -1;
+		}
+	}
+
+	// Lorsque le joueur revient dans la map
+	private void OnTriggerEnter(Collider collision){
+		if (collision.tag == "Equipe1" || collision.tag == "Equipe2") {
+			messOut.SetActive (false);
+			arrow.SetActive (false);
+			playerOut = 0;
+		}
+	}
+
+	// Recharge la scene au bout de 5 secondes
+	private IEnumerator outOfMap(){
+		// attend 5 secondes
+		//yield return new WaitForSeconds(5);
+
+		for (int i = 5; i >= 0; i--) {
+			messOut.transform.GetChild (1).GetComponent<Text> ().text = "00:0"+i;
+			yield return new WaitForSeconds (1);
+			if (playerOut == 0)
+				i = -1;
+			Debug.Log (i);
+		}
+
+		// si le joueur est toujours dehors
+		if(playerOut == -1){
+			// recharge la scene
+			Application.LoadLevel("ChoixEquipe");
+		}
+	}
+}
