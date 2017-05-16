0e5e137aedfc28cb39a1a1c07aa62b82cc499653
Nathan Urbain <nathan.urbain@etu.unistra.fr>
2017-05-16 14:31:16 +0200

    Ajout Menu Pause

---
+++ Build_Projet/Assets/ReseauScript/DisconnectClient.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class DisconnectClient : MonoBehaviour  {
+
+	private NetworkManager nm ;
+	// Use this for initialization
+	void Start () {
+		nm = GetComponent<NetworkManager> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void disconnectClient(GameObject client)
+	{
+		nm.playerPrefab = client;
+		nm.StopClient ();
+	}
+}
--- Build_Projet/Assets/Scripts/Menu/MenuPause.cs
+++ Build_Projet/Assets/Scripts/Menu/MenuPause.cs
@@ -1,18 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
 
-public class MenuPause : MonoBehaviour {
+public class MenuPause : NetworkBehaviour {
 
 	private bool isPaused = false;
 
+	public DisconnectClient nm ;
+	public GameObject prefabs; 
+
 	// Use this for initialization
 	void Start () {
-		
+		nm = GameObject.FindGameObjectWithTag ("Network").GetComponent<DisconnectClient>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (!isLocalPlayer)
+			return;
+				
 		if (Input.GetKeyDown (KeyCode.Escape))
 			isPaused = !isPaused;
 		}
@@ -20,14 +28,14 @@ public class MenuPause : MonoBehaviour {
 	void OnGUI(){
 		if (isPaused) {
 			if (GUI.Button (new Rect (Screen.width / 2 - 30, Screen.height / 2 - 20, 80, 40), "Continuer")) {
+				
 				isPaused = false;
 			}
-		/*	if (GUI.Button (new Rect (Screen.width / 2 - 30, Screen.height / 2 - 20, 80, 40), "(Un)mute")) {
-				AudioListener.pause = !AudioListener.pause;
-			}*/
+
 			if (GUI.Button (new Rect (Screen.width / 2 - 30, Screen.height / 2 + 40, 80, 40), "Quitter")) {
 				Application.Quit ();
 			}
 		}
 	}
+
 }
