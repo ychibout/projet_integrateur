af961741e478ea5d3ab73650d474feadabfd9b03
Nathan Urbain <nathan.urbain@etu.unistra.fr>
2017-05-16 23:29:07 +0200

    Quitter une partie

---
+++ Build_Projet/Assets/Scripts/Menu/FinGame.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+
+public class FinGame : MonoBehaviour {
+
+	private NetworkManager nm ;
+	// Use this for initialization
+	void Start () {
+		nm = GameObject.FindGameObjectWithTag ("Network").GetComponent<NetworkManager> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void quitter()
+	{
+		nm.StopServer();
+		SceneManager.LoadScene ("ChoixEquipe");
+	}
+}
